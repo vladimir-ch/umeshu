3a517d57eee2897136d77c7a116ead7e63f2c4cb
Vladimir Chalupecky <vladimir.chalupecky@gmail.com>
2013-04-12 17:26:55 +0900

    Created HDS Face base classes with IDs and intrusive list hooks

--- src/umeshu/HDS/HDS_face_base.h
+++ src/umeshu/HDS/HDS_face_base.h
@@ -1,5 +1,5 @@
 //
-//  Copyright (c) 2011-2012 Vladimir Chalupecky
+//  Copyright (c) 2011-2013 Vladimir Chalupecky
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to
@@ -19,10 +19,13 @@
 //  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 //  IN THE SOFTWARE.
 
-#ifndef __HDS_FACE_BASE_H_INCLUDED__
-#define __HDS_FACE_BASE_H_INCLUDED__
+#ifndef UMESHU_HDS_HDS_FACE_BASE_H
+#define UMESHU_HDS_HDS_FACE_BASE_H
 
-#include <boost/assert.hpp>
+#include "Identifiable.h"
+
+#include <boost/intrusive/list_hook.hpp>
+#include <boost/type_traits/integral_constant.hpp>
 
 namespace umeshu {
 namespace hds {
@@ -30,21 +33,90 @@ namespace hds {
 template <typename HDS>
 class HDS_face_base
 {
+
 public:
-  typedef typename HDS::Node_handle           Node_handle;
-  typedef typename HDS::Halfedge_handle       Halfedge_handle;
-  typedef typename HDS::Edge_handle           Edge_handle;
-  typedef typename HDS::Face_handle           Face_handle;
+
+  typedef typename HDS::Node_handle     Node_handle;
+  typedef typename HDS::Halfedge_handle Halfedge_handle;
+  typedef typename HDS::Edge_handle     Edge_handle;
+  typedef typename HDS::Face_handle     Face_handle;
+
+  HDS_face_base()
+    : adj_he_()
+  {}
 
   Halfedge_handle halfedge() const { return adj_he_; }
 
   void set_halfedge( Halfedge_handle he ) { adj_he_ = he; }
 
 private:
+
   Halfedge_handle adj_he_;
+
+};
+
+
+template <typename HDS>
+class HDS_face_base_with_id : public HDS_face_base<HDS>
+                            , public Identifiable
+{
+
+  typedef HDS_face_base<HDS> Base;
+
+public:
+
+  typedef boost::false_type Supports_intrusive_list;
+  typedef boost::true_type  Supports_id;
+
+  typedef typename Base::Node_handle     Node_handle;
+  typedef typename Base::Halfedge_handle Halfedge_handle;
+  typedef typename Base::Edge_handle     Edge_handle;
+  typedef typename Base::Face_handle     Face_handle;
+
+};
+
+
+template <typename HDS>
+class HDS_face_base_intrusive_list : public HDS_face_base<HDS>
+                                   , public boost::intrusive::list_base_hook<>
+{
+
+  typedef HDS_face_base<HDS> Base;
+
+public:
+
+  typedef boost::true_type  Supports_intrusive_list;
+  typedef boost::false_type Supports_id;
+
+  typedef typename Base::Node_handle     Node_handle;
+  typedef typename Base::Halfedge_handle Halfedge_handle;
+  typedef typename Base::Edge_handle     Edge_handle;
+  typedef typename Base::Face_handle     Face_handle;
+
+};
+
+
+template <typename HDS>
+class HDS_face_base_intrusive_list_with_id : public HDS_face_base<HDS>
+                                           , public boost::intrusive::list_base_hook<>
+                                           , public Identifiable
+{
+
+  typedef HDS_face_base<HDS> Base;
+
+public:
+
+  typedef boost::true_type Supports_intrusive_list;
+  typedef boost::true_type Supports_id;
+
+  typedef typename Base::Node_handle     Node_handle;
+  typedef typename Base::Halfedge_handle Halfedge_handle;
+  typedef typename Base::Edge_handle     Edge_handle;
+  typedef typename Base::Face_handle     Face_handle;
+
 };
 
 } // namespace hds
 } // namespace umeshu
 
-#endif /* __HDS_FACE_BASE_H_INCLUDED__ */
+#endif // UMESHU_HDS_HDS_FACE_BASE_H
